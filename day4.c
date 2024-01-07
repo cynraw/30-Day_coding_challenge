7d794413ac9c6ce4cafab40a03d39c4f5c1a51e4
cynraw <cynthiaronoh@gmail.com>
2024-01-07 22:26:35 +0300

    recursion in c

--- day4.c
+++ day4.c
@@ -2,13 +2,22 @@
 #include <string.h>
 
 int sum(int c); // function declaration
-
+/**
+ * main - Main function, entry and exit point of the program.
+ *
+ * return - 0 (success)
+ */
 int main(){
 	int result = sum(7);
 	printf("%d", result);
 	return 0;
 }
 
+/**
+ * sum - function to illustrate recursion in c
+ *
+ * return - integer
+ */
 int sum(int c){
 	if (c > 0)
 	{
