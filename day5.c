6d2ebef04d5aa78cb422b4d90a0a0a68eddf63ad
cynraw <cynthiaronoh@gmail.com>
2024-01-08 22:50:35 +0300

    strings intro

---
+++ day5.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+	char myName[] = "Cynthia Ronoh"; // creating a string
+	printf("my name is : %s", myName);
+	printf("%c", myName[3]); // accessing an element in a string
+return 0;
+}
+
+
+
